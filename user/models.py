26437a9c2821ec53874c1a4c77b9805e4043013f
Alho77 <a.hodavand@gmail.com>
2020-02-29 00:20:44 +0330

    initial phone and email models and update UserManager

--- user/models.py
+++ user/models.py
@@ -12,6 +12,17 @@ class UserManager(BaseUserManager):
         user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
+        if extra_fields.__contains__('phone'):
+            phone = extra_fields['phone']
+            for entry in phone:
+                Phone.objects.create(user=user, phone=entry)
+
+        if extra_fields.__contains__('email'):
+            email = extra_fields['email']
+            for entry in email:
+                Email.objects.create(user=user, email=entry)
+
         return user
 
     def create_superuser(self, username, password, **extra_fields):
@@ -23,12 +34,22 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
-    birth_data = models.DateField(auto_now=True, blank=True)
+    birth_data = models.DateField(blank=True, auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    phone = models.CharField(max_length=10, blank=True)
 
     objects = UserManager()
 
     def __str__(self):
         return self.username
+
+
+class Phone(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    phone = models.IntegerField(unique=True)
+
+
+class Email(models.Model):
+    user = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="Emails")
+    email = models.EmailField(unique=True, max_length=30)
