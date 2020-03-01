93ce5b117944c365b3b7223f2ad680de8f0b6926
Alho77 <a.hodavand@gmail.com>
2020-03-01 09:53:46 +0330

    initial test for duplicate data

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,20 +1,41 @@
 from django.contrib.auth import get_user_model
-from rest_framework.test import APITestCase
+from django.test import TestCase
+from django.db.utils import IntegrityError
 
+from user.models import Email, Phone
 
 User = get_user_model()
 
 
-class UserModelTest(APITestCase):
+class UserModelTest(TestCase):
 
     def test_create_user(self):
         """Test create user"""
-        payload = {'username': 'test', 'password': '1234'}
+        payload = {
+            'username': 'test',
+            'password': '1234',
+            'phone': ['12345678', '12345679'],
+            'email': ['test@test.com', ]
+        }
         user = User.objects.create_user(**payload)
 
         self.assertEqual(user.username, payload['username'])
         self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(len(Phone.objects.filter(user=user)), 2)
+        self.assertEqual(len(Email.objects.filter(user=user)), 1)
         self.assertTrue(user.check_password(payload['password']))
+
+    def test_create_user_duplicate(self):
+        """Test create user with duplicate entries for phone and email"""
+        payload = {
+            'username': 'test',
+            'password': '1234',
+            'phone': ['12345678', '12345678'],
+            'email': ['test@test.com', 'test@test.com']
+        }
+
+        with self.assertRaises(IntegrityError):
+            User.objects.create_user(**payload)
 
     def test_create_superuser(self):
         """Test create user"""
